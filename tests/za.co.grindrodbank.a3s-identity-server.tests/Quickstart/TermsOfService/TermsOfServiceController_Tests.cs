fdcff789550d95a3b2b2b74078989d0c5e2bc958
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2019-12-09 11:23:09 +0200

    Corrected all subclasses of SignInManager after breaking changes after upgrading to Asp.Net core 3.

--- tests/za.co.grindrodbank.a3s-identity-server.tests/Quickstart/TermsOfService/TermsOfServiceController_Tests.cs
+++ tests/za.co.grindrodbank.a3s-identity-server.tests/Quickstart/TermsOfService/TermsOfServiceController_Tests.cs
@@ -76,9 +76,10 @@ namespace za.co.grindrodbank.a3sidentityserver.tests.Quickstart.TermsOfService
             var mockLdapAuthenticationModeRepository = Substitute.For<LdapAuthenticationModeRepository>(fakeA3SContext, mockConfiguration);
             var mockLdapConnectionService = Substitute.For<LdapConnectionService>(mockLdapAuthenticationModeRepository, mockUserRepository);
             mockAuthenticationSchemeProvider = Substitute.For<IAuthenticationSchemeProvider>();
+            var mockUserConfirmation = Substitute.For<IUserConfirmation<UserModel>>();
 
             fakeSignInManager = new CustomSignInManagerFake<UserModel>(fakeUserManager, mockContextAccessor, mocClaimsFactory, mockOptionsAccessor, mockSignInLogger, fakeA3SContext,
-                mockAuthenticationSchemeProvider, mockLdapAuthenticationModeRepository, mockLdapConnectionService);
+                mockAuthenticationSchemeProvider, mockLdapAuthenticationModeRepository, mockLdapConnectionService, mockUserConfirmation);
 
             termsOfServiceRepository = Substitute.For<ITermsOfServiceRepository>();
             mockClientStore = Substitute.For<IClientStore>();
