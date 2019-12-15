5b1143a4ce8ec1b7486156794c6a1f21206ac909
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2019-12-15 14:09:40 +0200

    ApplicationServer and ApplicationController unit tests.

---
+++ tests/za.co.grindrodbank.a3s.tests/Controllers/ClientController_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Xunit;
+using za.co.grindrodbank.a3s.A3SApiResources;
+using za.co.grindrodbank.a3s.Controllers;
+using za.co.grindrodbank.a3s.Services;
+
+namespace za.co.grindrodbank.a3s.tests.Controllers
+{
+    public class ClientController_Tests
+    {
+        [Fact]
+        public async Task ListClientsAsync_WithNoInputs_ReturnsList()
+        {
+            // Arrange
+            var clientService = Substitute.For<IClientService>();
+
+            var inList = new List<Oauth2Client>();
+            inList.Add(new Oauth2Client { Name = "Test Client 1", ClientId = "test-client-1", AllowedOfflineAccess = true });
+            inList.Add(new Oauth2Client { Name = "Test Client 2", ClientId = "test-client-2", AllowedOfflineAccess = false });
+            inList.Add(new Oauth2Client { Name = "Test Client 3", ClientId = "test-client-3", AllowedOfflineAccess = true });
+
+            clientService.GetListAsync().Returns(inList);
+
+            var controller = new ClientController(clientService);
+
+            // Act
+            IActionResult actionResult = await controller.ListClientsAsync(0, 50, string.Empty, null);
+
+            // Assert
+            var okResult = actionResult as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var outList = okResult.Value as List<Oauth2Client>;
+            Assert.NotNull(outList);
+
+            for (var i = 0; i < outList.Count; i++)
+            {
+                Assert.Equal(outList[i].ClientId, inList[i].ClientId);
+                Assert.Equal(outList[i].Name, inList[i].Name);
+            }
+        }
+    }
+}
---
+++ tests/za.co.grindrodbank.a3s.tests/Services/ClientService_Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using IdentityServer4.EntityFramework.Entities;
+using NSubstitute;
+using Xunit;
+using za.co.grindrodbank.a3s.A3SApiResources;
+using za.co.grindrodbank.a3s.MappingProfiles;
+using za.co.grindrodbank.a3s.Repositories;
+using za.co.grindrodbank.a3s.Services;
+
+namespace za.co.grindrodbank.a3s.tests.Services
+{
+    public class ClientService_Tests
+    {
+        private readonly IMapper mapper;
+
+        public ClientService_Tests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new Oauth2ClientResourceClientModelProfile());
+            });
+
+            mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetList_GivenNoInput_ReturnsClientResourceList()
+        {
+            var clientRepository = Substitute.For<IIdentityClientRepository>();
+            var mockedClientModels = new List<Client>();
+
+            mockedClientModels.Add(new Client { ClientName = "Test Client 1", ClientId = "client-id-1" });
+            mockedClientModels.Add(new Client { ClientName = "Test Client 2", ClientId = "client-id-2" });
+            clientRepository.GetListAsync().Returns(mockedClientModels);
+
+            var clientService = new ClientService(clientRepository, mapper);
+            var clientsList = await clientService.GetListAsync();
+
+            var clientResource1 = clientsList.Find(am => am.Name == "Test Client 1");
+            Assert.True(clientResource1.GetType() != null, "Returned client resource 1 must not be null.");
+            Assert.True(clientResource1.GetType() == typeof(Oauth2Client), "Returned client resource 1 must be of type Oauth2Client.");
+            Assert.True(clientResource1.ClientId == "client-id-1", "Returned client resource 1 ClientId must be 'client-id-1'.");
+
+            var clientResource2 = clientsList.Find(am => am.Name == "Test Client 2");
+            Assert.True(clientResource2.GetType() != null, "Returned client resource 2 must not be null.");
+            Assert.True(clientResource2.GetType() == typeof(Oauth2Client), "Returned client resource 2 must be of type Oauth2Client.");
+            Assert.True(clientResource2.ClientId == "client-id-2", "Returned client resource 2 ClientId must be 'client-id-2'.");
+        }
+    }
+}
