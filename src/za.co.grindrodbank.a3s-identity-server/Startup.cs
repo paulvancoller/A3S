216aed270c668094348d1ae3bc907fcb3dbec622
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2019-12-20 06:10:40 +0200

    Removed SameSite cookie options for user agents that does not understand the new functionality.

--- src/za.co.grindrodbank.a3s-identity-server/Startup.cs
+++ src/za.co.grindrodbank.a3s-identity-server/Startup.cs
@@ -26,6 +26,7 @@ using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
+using Microsoft.AspNetCore.Http;
 
 namespace za.co.grindrodbank.a3sidentityserver
 {
@@ -53,29 +54,25 @@ namespace za.co.grindrodbank.a3sidentityserver
                 .AddUserStore<CustomUserStore>()
                 .AddDefaultTokenProviders();
 
-            services.ConfigureApplicationCookie(options =>
+            services.Configure<CookiePolicyOptions>(options =>
             {
-                // Cookie settings
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-                options.LoginPath = "/Account/Login";
-                options.SlidingExpiration = true;
-                options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
             });
 
             // Register own SignInManager to handle Just-In-Time LDAP Auth
             services.AddScoped<SignInManager<UserModel>, CustomSignInManager<UserModel>>();
 
-            services.AddRazorPages()
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
-                .AddNewtonsoftJson(options =>
-                 {
-                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                 });
-
-            services.AddControllers();
+            services.AddControllersWithViews()
+               .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
+               .AddNewtonsoftJson(options =>
+               {
+                   options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                   options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+               });
 
             services.Configure<IISOptions>(iis =>
             {
@@ -119,6 +116,7 @@ namespace za.co.grindrodbank.a3sidentityserver
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCookiePolicy();
 
             if (Environment.IsDevelopment())
             {
@@ -181,6 +179,46 @@ namespace za.co.grindrodbank.a3sidentityserver
                     context.SaveChanges();
                 }
             }
+        }
+
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+
+                if (DisallowsSameSiteNone(userAgent))
+                    options.SameSite = SameSiteMode.Unspecified;
+            }
+        }
+
+        public bool DisallowsSameSiteNone(string userAgent)
+        {
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+                return true;
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+                return true;
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+                return true;
+
+            return false;
         }
     }
 }
