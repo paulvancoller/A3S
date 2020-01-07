0cee8406d77df92d18519847c7f73cd75f0b7cbf
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2020-01-07 14:21:48 +0200

    Conversion from absolute to abstract implementation of LdapConnectionClient with use and test updates.

--- src/za.co.grindrodbank.a3s/ConnectionClients/ILdapConnectionClient.cs
+++ src/za.co.grindrodbank.a3s/ConnectionClients/ILdapConnectionClient.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using Novell.Directory.Ldap;
+
 namespace za.co.grindrodbank.a3s.ConnectionClients
 {
     public interface ILdapConnectionClient
     {
+        void Connect(string host, int port);
+        void Bind(string dn, string password);
+        LdapSearchResults Search(string @base, int scope, string filter, string[] attrs, bool typesOnly);
     }
 }
--- src/za.co.grindrodbank.a3s/ConnectionClients/LdapConnectionClient.cs
+++ src/za.co.grindrodbank.a3s/ConnectionClients/LdapConnectionClient.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using Novell.Directory.Ldap;
+
 namespace za.co.grindrodbank.a3s.ConnectionClients
 {
     public class LdapConnectionClient : ILdapConnectionClient
     {
+        private readonly LdapConnection ldapConnection;
+
         public LdapConnectionClient()
         {
+            ldapConnection = new LdapConnection();
+        }
+
+
+        ~LdapConnectionClient()
+        {
+            if (ldapConnection != null)
+                ldapConnection.Dispose();
+        }
+
+        public void Bind(string dn, string password)
+        {
+            ldapConnection.Bind(dn, password);
+        }
+
+        public void Connect(string host, int port)
+        {
+            ldapConnection.Connect(host, port);
+        }
+
+        public LdapSearchResults Search(string @base, int scope, string filter, string[] attrs, bool typesOnly)
+        {
+            return ldapConnection.Search(@base, scope, filter, attrs, typesOnly);
         }
     }
 }
