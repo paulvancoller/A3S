154663bc54ff3370fa741b8c720a3aa836a8e872
Victor Szewczenko <szewczenkov@gmail.com>
2020-01-10 10:20:34 +0200

    Adding some basic data models pertaining to profiles and sub-realms.

---
+++ src/za.co.grindrodbank.a3s/Models/SubRealm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace za.co.grindrodbank.a3s.Models
+{
+    public class SubRealm : AuditableModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+    }
+}
