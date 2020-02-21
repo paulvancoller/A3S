c4abed2f96d25c16f37805ec3f8f928d49a916b7
AntonRyzhkou <55533511+AntonRyzhkou@users.noreply.github.com>
2020-02-21 15:04:29 +0300

    Update schema to track User consent agreement/disagreement

--- db/sql/a3s/V04__consent_of_service.sql
+++ db/sql/a3s/V04__consent_of_service.sql
@@ -41,3 +41,130 @@ COMMENT ON COLUMN _a3s.consent_of_service.consent_file IS 'A .tar.gz file, conta
 
 ALTER TABLE ONLY _a3s.consent_of_service
     ADD CONSTRAINT consent_of_service_pk PRIMARY KEY (id);
+	
+------------ACCEPTANCE------------
+--
+-- Name: consent_of_service_user_acceptance; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+CREATE TABLE _a3s.consent_of_service_user_acceptance (
+    id uuid NOT NULL,
+	permission_id uuid NOT NULL,
+    user_id text NOT NULL,
+    acceptance_time tstzrange DEFAULT tstzrange(CURRENT_TIMESTAMP, NULL::timestamp with time zone) NOT NULL
+);
+
+
+ALTER TABLE _a3s.consent_of_service_user_acceptance OWNER TO postgres;
+
+--
+-- Name: TABLE consent_of_service_user_acceptance; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON TABLE _a3s.consent_of_service_user_acceptance IS 'This records the acceptance of consent of service entries by users.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance.permission_id; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance.permission_id IS 'User accepted the specific consent permission.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance.user_id; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance.user_id IS 'User ID accepted the specific consent permission.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance.acceptance_time; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance.acceptance_time IS 'The date and time the user accepted the specific consent.';
+
+--
+-- Name: consent_of_service consent_of_service_pk; Type: CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance
+    ADD CONSTRAINT consent_of_service_user_acceptance_pk PRIMARY KEY (id);
+
+--
+-- Name: consent_of_service_user_acceptance fk_terms_of_service_user_acceptance_history_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance
+    ADD CONSTRAINT fk_consent_of_service_user_acceptance_user_user_id FOREIGN KEY (user_id) REFERENCES _a3s.application_user(id) MATCH FULL;
+	
+--
+-- Name: consent_of_service_user_acceptance fk_terms_of_service_user_acceptance_history_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance
+    ADD CONSTRAINT fk_consent_of_service_user_acceptance_permission_permission_id FOREIGN KEY (permission_id) REFERENCES _a3s.permission(id) MATCH FULL;
+	
+------------ACCEPTANCE HISTORY------------
+--
+-- Name: consent_of_service_user_acceptance_history; Type: TABLE; Schema: _a3s; Owner: postgres
+--
+
+CREATE TABLE _a3s.consent_of_service_user_acceptance_history (
+    id uuid NOT NULL,
+	permission_id uuid NOT NULL,
+    user_id text NOT NULL,
+	action_type smallint NOT NULL,
+    action_time tstzrange DEFAULT tstzrange(CURRENT_TIMESTAMP, NULL::timestamp with time zone) NOT NULL
+);
+
+
+ALTER TABLE _a3s.consent_of_service_user_acceptance_history OWNER TO postgres;
+
+--
+-- Name: TABLE consent_of_service_user_acceptance_history; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON TABLE _a3s.consent_of_service_user_acceptance_history IS 'This stores the history of the acceptance of consent of service entries by users.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance_history.permission_id; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance_history.permission_id IS 'User action by the specific consent permission.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance_history.user_id; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance_history.user_id IS 'User ID accepted the specific consent permission.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance_history.action_type; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance_history.action_type IS 'The date and time the user accepted the specific agreement.';
+
+--
+-- Name: COLUMN consent_of_service_user_acceptance_history.action_time; Type: COMMENT; Schema: _a3s; Owner: postgres
+--
+
+COMMENT ON COLUMN _a3s.consent_of_service_user_acceptance_history.action_time IS 'The date and time the user accepted the specific agreement.';
+
+--
+-- Name: consent_of_service_user_acceptance_history terms_of_service_user_acceptance_history_pk; Type: CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance_history
+    ADD CONSTRAINT consent_of_service_user_acceptance_history_pk PRIMARY KEY (id);
+	
+	--
+-- Name: consent_of_service_user_acceptance_history fk_terms_of_service_user_acceptance_history_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance_history
+    ADD CONSTRAINT fk_consent_of_service_user_acceptance_history_user_user_id FOREIGN KEY (user_id) REFERENCES _a3s.application_user(id) MATCH FULL;
+	
+--
+-- Name: consent_of_service_user_acceptance_history fk_terms_of_service_user_acceptance_history_user_user_id; Type: FK CONSTRAINT; Schema: _a3s; Owner: postgres
+--
+
+ALTER TABLE ONLY _a3s.consent_of_service_user_acceptance_history
+    ADD CONSTRAINT fk_consent_of_service_user_acceptance_history_perm_perm_id FOREIGN KEY (permission_id) REFERENCES _a3s.permission(id) MATCH FULL;
+	
